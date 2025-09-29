583129234ccc4d878d39ecb7c2aac3ab1f9d3fd5
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2025-09-29 15:54:09 +0200

    refactor: restructure main entry points and environment setup
    - Moved main application logic to main_app.dart
    - Added main_dev.dart for development environment
    - Modified main_prod.dart to use AppConfig and ServiceLocator
    - Deleted api_constants.dart and locator.dart (functionality moved)
    - Updated widget_test.dart to import main_app.dart

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,25 +1,14 @@
 import 'package:flutter/material.dart';
-
 import 'features/crypto_list/presentaion/coin_list/screens/coin_list_screen.dart';
 
-void main() {
-  runApp(const Application());
-}
-
-class Application extends StatefulWidget {
+class Application extends StatelessWidget {
   const Application({Key? key}) : super(key: key);
 
-  @override
-  State<Application> createState() => _ApplicationState();
-}
-
-class _ApplicationState extends State<Application> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:CoinListScreen(),
+      home: CoinListScreen(),
     );
   }
 }
-
---
+++ lib/main_prod.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'config/app_config.dart';
+import 'core/di/service_locator.dart';
+import 'main_app.dart';
+
+void main() async {
+  AppConfig.setEnvironment(Environment.prod);
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await ServiceLocator.setup();
+  runApp(const Application());
+}
