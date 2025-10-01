0ca0badf67c108d63350aae7793e7d3347344869
MehrdadMoradi001 <moradi.mehrdad001@gmail.com>
2025-10-01 12:45:50 +0200

    refactor: update ApiKeyInterceptor to use Bearer token for apiKey

--- lib/core/network/api_key_interceptor.dart
+++ lib/core/network/api_key_interceptor.dart
@@ -1,20 +1,12 @@
 import 'package:dio/dio.dart';
 import '../../config/app_config.dart';
 
-// این کلاس مسئول اضافه کردن apiKey به تمام درخواست‌هاست
 class ApiKeyInterceptor extends Interceptor {
   @override
   void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    // پارامترهای فعلی درخواست را کپی می‌کنیم
-    final queryParameters = Map<String, dynamic>.from(options.queryParameters);
+    // اضافه کردن کلید به Header به عنوان Bearer Token
+    options.headers['Authorization'] = 'Bearer ${AppConfig.config.apiKey}';
 
-    // apiKey را به پارامترها اضافه می‌کنیم
-    queryParameters['apiKey'] = AppConfig.config.apiKey;
-
-    // پارامترهای جدید را به درخواست اعمال می‌کنیم
-    options.queryParameters = queryParameters;
-
-    // درخواست را به مرحله بعد ارسال می‌کنیم
     super.onRequest(options, handler);
   }
 }
